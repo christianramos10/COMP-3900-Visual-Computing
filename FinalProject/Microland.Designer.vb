db6bf2f782663441fa2ca4749d022669f668d325
Christian Ramos <christianramoslopez10@gmail.com>
2022-11-17 17:55:01 -0400

    Updated Project

---
+++ FinalProject/Microland.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Microland
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.outOfStockBtn = New System.Windows.Forms.Button()
+        Me.billBtn = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'outOfStockBtn
+        '
+        Me.outOfStockBtn.Location = New System.Drawing.Point(62, 12)
+        Me.outOfStockBtn.Name = "outOfStockBtn"
+        Me.outOfStockBtn.Size = New System.Drawing.Size(121, 53)
+        Me.outOfStockBtn.TabIndex = 0
+        Me.outOfStockBtn.Text = "Out of Sotck" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Items"
+        Me.outOfStockBtn.UseVisualStyleBackColor = True
+        '
+        'billBtn
+        '
+        Me.billBtn.Location = New System.Drawing.Point(206, 12)
+        Me.billBtn.Name = "billBtn"
+        Me.billBtn.Size = New System.Drawing.Size(121, 53)
+        Me.billBtn.TabIndex = 1
+        Me.billBtn.Text = "Bill for Today's " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Orders"
+        Me.billBtn.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(12, 81)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(361, 304)
+        Me.ListBox1.TabIndex = 2
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(398, 22)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 25
+        Me.DataGridView1.Size = New System.Drawing.Size(634, 350)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'Microland
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1044, 397)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.billBtn)
+        Me.Controls.Add(Me.outOfStockBtn)
+        Me.Name = "Microland"
+        Me.Text = "Microland"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents outOfStockBtn As Button
+    Friend WithEvents billBtn As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ FinalProject/Microland.vb
@@ -0,0 +1,185 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Windows.Forms
+Public Class Microland
+    Dim con As New SqlConnection("Data Source=DESKTOP-ETTN470;Initial Catalog=Microland;User ID=admin;Password=12345")
+    Dim cmd As New SqlCommand()
+    Dim adapter As New SqlDataAdapter()
+    Dim dt As New DataTable()
+    Dim dr As SqlDataReader
+    Public Sub Microland_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub outOfStockBtn_Click(sender As Object, e As EventArgs) Handles outOfStockBtn.Click
+        con.Open()
+
+        Dim str1 As String = "Here are the items that are out of inventory or must be reordered."
+        Dim str2 As String = "The numbers shown give the minimum reorder quantity required."
+
+        Dim query As String = "select * from Inventory where quantity <= 5"
+        cmd.Connection = con
+        cmd.CommandText = query
+        dr = cmd.ExecuteReader()
+
+        ListBox1.Items.Add(str1)
+        ListBox1.Items.Add(" ")
+        ListBox1.Items.Add(str2)
+        ListBox1.Items.Add(" ")
+
+        Dim displayStr As String
+
+        While (dr.Read())
+            displayStr = dr("itemID") + " "
+
+            Select Case dr("itemID")
+                Case "SW101"
+                    displayStr += Str(2) + " "
+                Case "SW109"
+                    displayStr += Str(0) + " "
+                Case "SW934"
+                    displayStr += Str(5) + " "
+                Case "HW913"
+                    displayStr += Str(5) + " "
+                Case "PL208"
+                    displayStr += Str(2) + " "
+                Case Else
+                    displayStr += Str(3) + " "
+            End Select
+
+            displayStr += dr("itemDescription")
+            ListBox1.Items.Add(displayStr)
+            displayStr = ""
+        End While
+
+        con.Close()
+
+    End Sub
+
+    Private Sub billBtn_Click(sender As Object, e As EventArgs) Handles billBtn.Click
+        con.Open()
+        cmd.Connection = con
+
+        ' Select distinct ids from orders
+        Dim query As String = "select distinct custID from Orders"
+        cmd.CommandText = query
+        dr = cmd.ExecuteReader
+
+        ' Save each customer ids
+        Dim ordersCustID As String = ""
+        While (dr.Read())
+            ordersCustID += Str(dr("custID")) + " "
+        End While
+        dr.Close()
+
+        Dim custIdArr() As String
+        custIdArr = ordersCustID.Split(" ")
+
+        'Iterate by customer
+        For Each cust As String In custIdArr
+            If Not (cust = " " Or cust = "") Then
+                'Get customer's information
+                query = "select * from Customers where custID = '" + cust + "'"
+                cmd.CommandText = query
+                dr = cmd.ExecuteReader
+
+                While (dr.Read())
+                    ListBox1.Items.Add(dr("custName"))
+                    ListBox1.Items.Add(dr("street"))
+                    ListBox1.Items.Add(dr("city"))
+                End While
+                dr.Close()
+
+                'Get number of orders from customer
+                query = "select quantity from Orders where custID = '" + cust + "' order by itemID"
+                cmd.CommandText = query
+                dr = cmd.ExecuteReader
+                Dim numOrders As Integer = 0
+                While (dr.Read())
+                    numOrders = numOrders + 1
+                End While
+                dr.Close()
+
+                'Initialize arrays
+                Dim itemLineQuantity(numOrders) As String
+                Dim itemLineDesc As String
+                Dim itemLinePrice As String
+
+                'Add customer's item quantity
+                query = "select quantity from Orders where custID = '" + cust + "' order by itemID"
+                cmd.CommandText = query
+                dr = cmd.ExecuteReader
+                Dim index As Integer = 0
+                While (dr.Read())
+                    itemLineQuantity(index) = Str(dr("quantity"))
+                    index = index + 1
+                End While
+                dr.Close()
+
+                ListBox1.Items.Add(itemLineQuantity(0))
+
+
+                'Add quantity, description and price to arrays, then print accordingly
+                'Add quantity, description and price to arrays, then print accordingly
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                'Get item's information
+                query = "select itemID from Orders where custID = '" + cust + "' order by itemID"
+                cmd.CommandText = query
+                dr = cmd.ExecuteReader
+                Dim items As String = ""
+                While (dr.Read())
+                    items += dr("itemID") + " "
+                End While
+                dr.Close()
+
+                Dim itemArr() As String
+                Dim itemDescLine As String = ""
+                Dim itemPriceLine As String = ""
+                itemArr = items.Split(" ")
+
+                For Each item As String In itemArr
+                    query = "select * from Inventory where itemID = '" + item + "'"
+                    cmd.CommandText = query
+                    dr = cmd.ExecuteReader
+                    While (dr.Read())
+                        itemDescLine += dr("itemDescription") + " "
+                        itemPriceLine += Str(dr("price")) + " "
+                    End While
+                    dr.Close()
+                Next
+
+                dr.Close()
+
+
+
+
+
+
+
+
+
+
+            End If
+        Next
+        con.Close()
+    End Sub
+
+
+End Class
